d23f1b8b528ded5a12c8ad09d075f9f73db3fd21
scienceopen <scienceopen@users.noreply.github.com>
2015-06-10 10:53:49 -0400

    initial commit

---
+++ polar_plot_matplotlib.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+demo of polar plots in Python Matplotlib
+Michael Hirsch
+Ref: http://stackoverflow.com/questions/18721762/matplotlib-polar-plot-is-not-plotting-where-it-should
+
+This example is for an azimuth/elevation plot.
+Azimuth [0,360) with north at 0 degrees
+Elevation [0,90)
+"""
+
+"""
+We have to reverse direction of "elevation"
+"""
+from numpy import array,radians
+from matplotlib.pyplot import figure,show
+
+def polarplot(az,el):
+    """
+    I use astype(float) because some programs give off lists or object arrays that radians() doesn't like
+    """
+    az = radians(az.astype(float))
+    el = 90-el.astype(float)
+
+    ax2=figure(2).gca(polar=True)
+    ax2.plot(az,el, marker='o',linestyle='.')
+    ax2.set_theta_zero_location('N')
+    ax2.set_theta_direction(-1)
+    ax2.set_yticks(range(0, 90+10, 10))                   # Define the yticks
+    yLabel = ['90', '', '', '60', '', '', '30', '', '', '']
+    ax2.set_yticklabels(yLabel)
+
+if __name__ == '__main__':
+
+    # arbitrary test numbers
+    az = array([10,50,30,185,273])
+    el = array([30,5, 85,36, 29])
+
+    polarplot(az,el)
+    show()
