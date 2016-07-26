58eef0cef677ff5df212a6993fd4e5a5dd4fd93f
Michael H <nobody@github.com>
2016-07-26 01:17:18 -0400

    update api

---
+++ xarray_matplotlib.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from datetime import datetime
+from matplotlib.pyplot import figure,show
+#%% generate data
+from numpy import arange
+from numpy.random import randn
+t = arange('2010-05-04T12:05','2010-05-04T12:16',dtype='datetime64[s]')
+y = randn(t.size)
+
+T = t.astype(datetime)
+ax = figure().gca()
+ax.plot(T,y)
+
+show()
