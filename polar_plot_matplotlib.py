4448e68a0dd09e88696a532411c9cc94cda2f5f6
scienceopen <scienceopen@users.noreply.github.com>
2016-07-28 11:11:08 -0400

    programmatic polar plot labeling

--- polar_plot_matplotlib.py
+++ polar_plot_matplotlib.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
 demo of polar plots in Python Matplotlib
 Michael Hirsch
@@ -7,35 +7,42 @@ Ref: http://stackoverflow.com/questions/18721762/matplotlib-polar-plot-is-not-pl
 This example is for an azimuth/elevation plot.
 Azimuth [0,360) with north at 0 degrees
 Elevation [0,90)
-"""
 
+We reverse direction of "elevation" to make center of plot 90 deg.
 """
-We have to reverse direction of "elevation"
-"""
-from numpy import array,radians,atleast_1d
+import numpy as np
 from matplotlib.pyplot import figure,show
 
-def polarplot(az,el):
-    #copied from satkml
+def polarplot(az,el,minel=0.,delv=10.):
     """
-    I use astype(float) because some programs give off lists or object arrays that radians() doesn't like
-    """
-    az = radians(atleast_1d(az).astype(float))
-    el = 90 - atleast_1d(el).astype(float)
+    az: azimuths of points to plot (degrees)
+    el: elevation (above horizon) of points to plot (degrees)
 
-    ax2=figure().gca(polar=True)
-    ax2.plot(az,el, marker='o',linestyle='none')
-    ax2.set_theta_zero_location('N')
-    ax2.set_theta_direction(-1)
-    ax2.set_yticks(range(0, 90+10, 10))                   # Define the yticks
-    yLabel = ['90', '', '', '60', '', '', '30', '', '', '']
-    ax2.set_yticklabels(yLabel)
+    minel: minimum elevation angle (degrees) to plot (axes limit)
+    delv: increment to label elevation angles (degrees)
+    """
+
+    # astype(float) because some programs give off lists or object arrays that radians() doesn't like
+    az = np.radians(np.asarray(az).astype(float))
+    el = 90. - np.asarray(el).astype(float)
+
+    ax = figure().gca(polar=True) # Note the polar=True keyword
+
+    ax.plot(az,el, marker='o',linestyle='none') # you can use other markers or linestyles too
+#%% boilerplate
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+
+    yl = np.arange(0., 90.-minel+delv, delv)
+    ylbl = (yl[::-1]+minel).astype(int).astype(str)
+    ax.set_yticks(yl)
+    ax.set_yticklabels(ylbl)
 
 if __name__ == '__main__':
 
     # arbitrary test numbers
-    az = array([10,50,30,185,273])
-    el = array([30,5, 80,36, 29])
+    az = [10,50,30,185,273]
+    el = [30,5, 80,36, 29]
 
     polarplot(az,el)
     show()
