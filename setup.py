832209abad2bea3f2f47a695bcb9cab0bc4d09c0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-24 10:13:54 -0500

    pep8, standard syntax

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-install_requires= ['numpy','h5py','matplotlib','bokeh']
+install_requires = ['numpy', 'h5py', 'matplotlib', 'bokeh']
 
 # %%
 from setuptools import setup, find_packages
 
 setup(name='python-matlab-examples',
-        packages = find_packages(),
-	    description='Tricks and tips with Matlab and Python',
-	    author='Michael Hirsch, Ph.D.',
-	    url='https://github.com/scivision/python-matlab-examples',
-        install_requires=install_requires,
-	  )
+      packages = find_packages(),
+      version = '0.1.0',
+      description='Tricks and tips with Matlab and Python',
+      author='Michael Hirsch, Ph.D.',
+      url='https://github.com/scivision/python-matlab-examples',
+      install_requires=install_requires,
+      python_requires='>=3.4'-)
--- title_gitrev.py
+++ title_gitrev.py
@@ -3,7 +3,8 @@
 example of putting git short revision in matplotlib plot, up in the corner
 (rather than in title where git revision text is too large)
 
-This is helpful for when a colleague wants a plot exactly recreated from a year ago, to help find the exact code used to create that plot
+This is helpful for when a colleague wants a plot exactly recreated from a year ago,
+ to help find the exact code used to create that plot.
 
 http://matplotlib.org/api/figure_api.html
 """
@@ -11,14 +12,15 @@ import subprocess
 from matplotlib.pyplot import figure,show
 
 try:
-    gitrev = subprocess.check_output(['git','rev-parse','--short','HEAD']).decode('utf8').strip('\n')
+    gitrev = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'],
+                                     universal_newlines=True).strip('\n')
 except Exception: # maybe they don't have git installed
     gitrev = ''
 
 fg = figure()
 ax = fg.gca()
-ax.plot([1,2])
+ax.plot([1, 2])
 ax.set_title('my cool plot')
-fg.text(1.,1.,'git: '+gitrev,ha='right',va='top',rotation='vertical')
+fg.text(1., 1., 'git: ' + gitrev, ha='right', va='top', rotation='vertical')
 
 show()
