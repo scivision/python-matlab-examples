439ce38b48562ff387e658bc78ae209e27c6db3a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-24 13:11:44 -0500

    template

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
+.. image:: https://travis-ci.org/scivision/python-matlab-examples.svg?branch=master
+    :target: https://travis-ci.org/scivision/python-matlab-examples
+    
 ===============
 python-learning
 ===============
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 install_requires = ['numpy', 'h5py', 'matplotlib>=2.1', 'bokeh']
-
+tests_require = ['nose','coveralls']
 # %%
 from setuptools import setup, find_packages
 
@@ -8,7 +8,10 @@ setup(name='python-matlab-examples',
       packages = find_packages(),
       version = '0.1.0',
       description='Tricks and tips with Matlab and Python',
+      long_description=open('README.rst').read(),
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/python-matlab-examples',
       install_requires=install_requires,
+      tests_require=tests_require,
+      extras_require={'tests':tests_require},
       python_requires='>=3.4')
