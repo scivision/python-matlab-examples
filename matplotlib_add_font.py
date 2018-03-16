6c8596840426937288846305ce6801f9596174bf
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-16 11:58:58 -0400

    not working

---
+++ matplotlib_add_font.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Add fonts to Matplotlib (for any operating system)
+
+1. add the new font to your operating system.
+   In this example, I'll add a Chinese font to Ubuntu::
+
+       apt install fonts-wqy-zenhei
+2. Update Matplotlib font cache with this script.
+
+HOWEVER, this didn't work for me on multiple PCs with Matplotlib 2.2.0
+
+See https://github.com/scivision/histfeas/blob/master/Plots/FirstAuroralConjugate.py
+"""
+import matplotlib
+# %% remove old font cache
+cachepath = matplotlib.get_cachedir()
+
+
+matplotlib.font_manager._rebuild()
