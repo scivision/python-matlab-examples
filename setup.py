98ae3f1949e819573e7c4f738954a170efd5ac9e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-26 18:57:15 -0400

    update datetime64 with imshow example for mpl 2.2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-install_requires = ['numpy', 'h5py', 'matplotlib>=2.1', 'bokeh','mpld3','astropy','scipy',
+install_requires = ['numpy', 'h5py', 'matplotlib>=2.2', 'bokeh','mpld3','astropy','scipy',
             'oct2py']
-tests_require = ['nose','coveralls']
+tests_require = ['pytest','nose','coveralls']
 # %%
 from setuptools import setup, find_packages
 
 setup(name='python-matlab-examples',
       packages = find_packages(),
-      version = '0.1.0',
+      version = '0.5.0',
       description='Tricks and tips with Matlab and Python',
       long_description=open('README.rst').read(),
       author='Michael Hirsch, Ph.D.',
@@ -15,4 +15,17 @@ setup(name='python-matlab-examples',
       install_requires=install_requires,
       tests_require=tests_require,
       extras_require={'tests':tests_require},
-      python_requires='>=3.4')
+      python_requires='>=3.4',
+      classifiers=['
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Scientific/Engineering :: Visualization',
+     ],
+     include_package_data=True,
+     )
--- xarray_matplotlib.py
+++ xarray_matplotlib.py
@@ -4,14 +4,46 @@ matplotlib with datetime64 testing
 """
 from datetime import datetime
 from matplotlib.pyplot import figure,show
-#%% generate data
-from numpy import arange
-from numpy.random import randn
-t = arange('2010-05-04T12:05','2010-05-04T12:16', dtype='datetime64[s]')
-y = randn(t.size)
+import matplotlib.dates as mdates
+import numpy as np
 
-T = t.astype(datetime)
-ax = figure().gca()
-ax.plot(T,y)
 
-show()
+def atest_plot2d_datetime():
+    t = np.arange('2010-05-04T12:05:00','2010-05-04T12:05:01', dtype='datetime64[ms]')
+    y = np.random.randn(t.size)
+
+    #t = t.astype(datetime)  # Matplotlib < 2.2
+
+    ax = figure().gca()
+    ax.plot(t,y)
+
+
+def test_imshow_datetime():
+    """
+    keogram using matplotlib imshow
+    """
+    Ny = 500  # arbitrary
+
+    t = np.arange('2010-05-04T12:05','2010-05-04T12:06', dtype='datetime64[s]').astype(datetime)
+    im = np.random.random((Ny, t.size))
+    y = range(t.size) # arbitrary
+
+    mt = mdates.date2num((t[0],t[-1]))
+
+    fig = figure()
+    ax = fig.gca()
+    ax.imshow(im, extent=[mt[0],mt[1], y[0],y[-1]], aspect='auto')
+# %% datetime formatting
+    ax.xaxis_date()  # like "num2date"
+
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+
+
+    fig.autofmt_xdate()
+
+
+if __name__ == '__main__':
+    np.testing.run_module_suite()
+
+
+    show()
